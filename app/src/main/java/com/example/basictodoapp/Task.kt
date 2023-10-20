a4bffd7ab2b0c83ed6c415a7f3e3aabc95c87dc6
Ivan Espinosa <ivarespar@gmail.com>
2023-10-19 23:33:49 -0500

    [o16] Creacion interface: lista vertical tareas hardcode

---
+++ app/src/main/java/com/example/basictodoapp/Task.kt
@@ -0,0 +1,5 @@
+package com.example.basictodoapp
+
+data class Task(val name: String, val category: TaskCategory, val isSelected: Boolean =false) {
+
+}
--- app/src/main/java/com/example/basictodoapp/TodoActivity.kt
+++ app/src/main/java/com/example/basictodoapp/TodoActivity.kt
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.basictodoapp.TaskCategory.*
 
 class TodoActivity : AppCompatActivity() {
 
     private val categories = listOf(
-        TaskCategory.Personal,
-        TaskCategory.Business,
-        TaskCategory.Other
+        Personal,
+        Business,
+        Other
     )
 
-    private lateinit var rvCategories: RecyclerView;
-    private lateinit var categoriesAdapter: CategoriesAdapter;
+    private val tasks = mutableListOf(
+        Task("PruebaBusiness", Business),
+        Task("PruebaPersonal", Personal),
+        Task("PruebaOther", Other)
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +34,18 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
+
 
     }
 }
